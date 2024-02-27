4770dda68c76e1d06294c2f639923bc856a534dd
Connor Allen <connor27allen@gmail.com>
2024-02-27 12:03:47 -0500

    ready to push

---
+++ client/src/components/NoteForm.jsx
@@ -0,0 +1,70 @@
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+
+function NoteForm({
+  editNote,
+  setEditNote,
+  setShowNoteForm,
+  setNotes }) {
+  const [noteText, setNoteText] = useState('')
+
+  useEffect(() => {
+    if (editNote) {
+      setNoteText(editNote.text)
+    }
+  }, [])
+
+  const createOrEditNote = async (e) => {
+    e.preventDefault()
+
+
+    if (!editNote) {
+      const res = await axios.post('/api/notes', {
+        text: noteText
+      })
+
+      setNotes((oldState) => {
+        return [...oldState, res.data]
+      })
+    } else {
+      await axios.put('/api/note', {
+        note_id: editNote._id,
+        text: noteText
+      })
+
+      
+      setNotes((oldState) => {
+
+        editNote.text = noteText
+
+        return [...oldState]
+      })
+    }
+    setShowNoteForm(false)
+    setEditNote(null)
+  }
+
+  const closeModal = () => setShowNoteForm(false)
+
+  const handleInputChange = (e) => {
+    setNoteText(e.target.value)
+  }
+
+  return (
+    <div className="note-form">
+      <h1 className="text-center">{editNote ? 'Edit' : 'Create'} Note Form</h1>
+
+      <form onSubmit={createOrEditNote} className="column">
+        <input
+          value={noteText}
+          onChange={handleInputChange}
+          type="text"
+          placeholder="Enter the note text"></input>
+        <button>{editNote ? 'Save' : 'Create'}</button>
+        <button onClick={closeModal} className="cancel-btn">Cancel</button>
+      </form>
+    </div>
+  )
+}
+
+export default NoteForm
